d2d6810d2bd56c4e6eeece4fbef6154b088c3a1d
Karen-1403 <karenmourice@gmail.com>
2025-05-01 05:08:16 +0300

    New feautures added in inventory page: adding new items and adding new categories now available

--- Test/inventory/urls.py
+++ Test/inventory/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('',views.account_page,name='Inventory'),
     path('item_detail_<int:id>/', views.item_detail, name='item_detail'),
+    path('add_item/',views.add_item, name='add_item'),
+    path('add-category/', views.add_category, name='add_category'),
 
     
 ]
